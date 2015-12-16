de0e5bd4fdd2968c65c8cad6af877b38658b1318
Brandi Soucy <bbsoucy@gmail.com>
2015-12-16 15:59:26 -0600

    User can see the input and recipe card

--- tests/acceptance/recipes/new-test.js
+++ tests/acceptance/recipes/new-test.js
@@ -11,29 +11,30 @@ test('User can visit a recipe page and see a recipe', function(assert) {
   });
 });
 
-test('User can see a list of recipe-items', function(assert) {
+test('User can see a basic recipe list of recipe-items', function(assert) {
   visit('/');
 
   fillIn('.recipe-items', '1 Pound Chicken');
 
   andThen(function() {
     var recipeItem = findWithAssert('.recipe-items');
-    assert.equal(recipeItem.length, 7);
-  });
-});
-
-test('User can change the serving size of the recipe', function(assert) {
-  let number = '.num-servings-input';
-
-  fillIn(number, 8);
-
-  click('.adjust-recipe-submit');
-
-  andThen(function() {
     var numServings = findWithAssert('.num-servings-input');
-    findWithAssert('.num-servings-input');
 
+    assert.equal(recipeItem.length, 7);
     assert.equal(numServings.length, 1);
-    assert.equal(numServings.eq(0).val(), 8);
+    assert.equal(numServings.eq(0).val(), 8, 'Input starts with a value of 8');
   });
 });
+
+// test('User can change the serving size of the recipe', function(assert) {
+//   visit('/');
+//   let number = '.num-servings-input';
+//
+//   fillIn(number, 8);
+//
+//   click('.adjust-recipe-submit');
+//
+//   andThen(function() {
+//
+//   });
+// });
