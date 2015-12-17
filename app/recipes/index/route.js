08bcd35961694c0fd5d8d766e131d578843878c9
Brandi Soucy <bbsoucy@gmail.com>
2015-12-16 22:16:46 -0600

    Setting up model and template

--- app/recipes/index/route.js
+++ app/recipes/index/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+        {amount: '1 lb', name: 'Chicken'},
+        {amount: '1 Cup', name: 'Sliced Carrots'},
+        {amount: '0.5 Cup', name: 'Sliced Celery'},
+        {amount: '0.75 Cup', name: 'Butter'},
+        {amount: '0.75 Cup', name: 'Chopped Onions'},
+        {amount: '0.25 Cup', name: 'Flour'},
+        {amount: '2', name: 'Pie Crusts'},
+    ];
+  },
 });
